6e8fadaeaeb718ba7a1bff9759bb3cbd6e9d3a17
Ayslan Marques <ayslan.marques@sistemasseguros.local>
2017-08-04 15:03:35 -0300

    Aula de módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,19 @@ import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
+import { CursosModule } from './cursos/cursos.module';
+
 @NgModule({
+  //Componentes, diretivas e pipes
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
     MeuPrimeiro2Component
   ],
+  //Outros módulos
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
